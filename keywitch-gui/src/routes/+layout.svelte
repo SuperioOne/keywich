a61e7cbd839ec6fc1fee87cf81a03436a7e0daab
Timur Olur <timurolur@gmail.com>
2023-04-14 04:50:50 +0300

    added skeleton ui

--- keywitch-gui/src/routes/+layout.svelte
+++ keywitch-gui/src/routes/+layout.svelte
@@ -1,3 +1,35 @@
+<script lang="ts">
+  import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+  import "@skeletonlabs/skeleton/styles/all.css";
+  import "../app.postcss";
+  import "../app.css"
 
+  import type {Writable} from "svelte/store";
+  import {AppRail, AppRailTile} from "@skeletonlabs/skeleton";
+  import {AppShell} from '@skeletonlabs/skeleton';
+  import {writable} from "svelte/store";
 
-<slot />
+  const pageId: Writable<number> = writable(0);
+</script>
+
+<style>
+    html, body {
+        @apply h-full overflow-hidden;
+    }
+</style>
+
+<AppShell class="h-screen">
+    <svelte:fragment slot="sidebarLeft">
+        <AppRail selected={pageId}>
+            <AppRailTile tag="a" href="/" label="Home" value={0}>(icon)</AppRailTile>
+            <AppRailTile tag="a" href="/history" label="History" value={1}>(icon)</AppRailTile>
+            <AppRailTile tag="a" href="/settings" label="Settings" value={2}>(icon)</AppRailTile>
+        </AppRail>
+    </svelte:fragment>
+    <div class="p-10 flex align-middle justify-center">
+        <div class="flex-row w-full max-w-screen-lg">
+            <slot/>
+        </div>
+    </div>
+</AppShell>
+
---
+++ keywitch-gui/src/routes/history/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  const sourceData = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  ];
+</script>
+
+<div class="table-container">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Weight</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each sourceData as row, i}
+            <tr>
+                <td>{row.position}</td>
+                <td>{row.name}</td>
+                <td>{row.symbol}</td>
+                <td>{row.weight}</td>
+            </tr>
+        {/each}
+        </tbody>
+        <tfoot>
+        <tr>
+            <th colspan="3">Calculated Total Weight</th>
+            <td>12345</td>
+        </tr>
+        </tfoot>
+    </table>
+</div>
